da9122574aeef614a2b0baeddfbcd11f3a49c5e9
Mithilesh kumar <mkumar199861@gmail.com>
2021-09-21 19:28:07 +0530

    Made api end point to check health

--- main2.go
+++ main2.go
@@ -1,5 +1,7 @@
 package main
 import "fmt"
+import "time"
+import "sync"
 func todo(){
 	// arr :=[]int{1,2,4,6,7,8}
 	arr1:=[]string{"mithilesh","kumar","raghav"}
@@ -49,31 +51,84 @@ type Car struct{
 
 }
 
-func main() {
-	makedict:=make(map[string] interface{})
-	makedict["Name"]="Mithilesh"
-	makedict["Age"]=23
-	makedict["Car"]="Swift vdi"
-	fmt.Println(makedict)
-	l:=lambo{"pro"}
-	a:=Audi{"A8"}
-	l.Drive()
-	a.Drive()
+func heavy(){
+	for{
+	time.Sleep(time.Second*1)
+	fmt.Println("Heavy is called ")
+	}
+}
+func superheavy(){
+	for{
+	time.Sleep(time.Second*2)
+	fmt.Println("Super heavy called")
+	}
+}
+// func main() {
+// 	// var ch chan int
+// 	ch:=make(chan int)
+// 	chb:=make(chan int,3)
+// 	fmt.Println(ch)
+// 	//Send in a go routine 
+// 	go func(){
+// 		chb <- 1
+// 		chb <- 2
+// 		chb <- 3
+// 		chb <- 4
+// 		close(chb)
+
+// 	}()
+// 	for i :=range chb{
+// 		fmt.Println(i)
+// 	}
+
+// 	//Sniff from channel
+// 	// val:=<-ch
+// 	// fmt.Println("Value from channel",val)
+	
+// 	// go heavy()
+// 	// go superheavy()
+// 	//Add done wait 
+// 	wg:=&sync.WaitGroup{}
+// 	//mutexes (Critical Section)
+// 	wg.Add(2)
+// 	go func ()  {
+// 		fmt.Println("Hello")
+// 		wg.Done()
+		
+// 	}()
+// 	go func ()  {
+// 		fmt.Println("word")
+// 		wg.Done()
+		
+// 	}()
+// 	fmt.Println("Fin")
+// 	wg.Wait()
+
+// 	makedict:=make(map[string] interface{})
+// 	makedict["Name"]="Mithilesh"
+// 	makedict["Age"]=23
+// 	makedict["Car"]="Swift vdi"
+// 	fmt.Println(makedict)
+// 	l:=lambo{"pro"}
+// 	a:=Audi{"A8"}
+// 	l.Drive()
+// 	a.Drive()
 	
 
-	m1:=2
-	m2:=3
-    c:=Car {
-		Name:   "Audi",
-		Age: 2,
-		ModelNo: 2020,
-	}
-	fmt.Println(c)
-	fmt.Println(c.GetName())
-	c.Driving()
-	// fmt.Println(m1,m2)
-	// todo()
-	swap(&m1,&m2)
-	fmt.Println(m1,m2)
+// 	m1:=2
+// 	m2:=3
+//     c:=Car {
+// 		Name:   "Audi",
+// 		Age: 2,
+// 		ModelNo: 2020,
+// 	}
+// 	fmt.Println(c)
+// 	fmt.Println(c.GetName())
+// 	c.Driving()
+// 	// fmt.Println(m1,m2)
+// 	// todo()
+// 	swap(&m1,&m2)
+// 	fmt.Println(m1,m2)
+// 	// select {}
 
-}
+// }
